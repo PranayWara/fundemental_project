a7cd319fb3cd73db18fb563dc3545db4e7a31b13
Pranay Wara <pranay.wara@gmail.com>
2021-07-19 15:03:15 +0000

    PW: validators

--- application/forms.py
+++ application/forms.py
@@ -1,15 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.fields.core import SelectField
+from wtforms.validators import DataRequired, Length, ValidationError
+
 
 class CompanyFrom(FlaskForm):
-    name = StringField('Input or Update the Name of video Studio Company')
+    name = StringField('Input or Update the Name of video Studio Company', validators=[
+        DataRequired()])
     description = StringField('Add or Update a description of the Company')
     founders = StringField ('Input or Update the Name of the founder')
     submit = SubmitField()
 
 class GamesForm(FlaskForm):
-    name = StringField('Input or Update the Name of the Game')
+    name = StringField('Input or Update the Name of the Game', validators=[
+        DataRequired()])
     genre = StringField('Add or Update the genre of the Game')
     company = SelectField('Choose a Company', choices=[])
     submit = SubmitField()
